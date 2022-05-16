ff3fa48adc71e37929cc194d14c156fa075a198f
David Jegat <david.jegat@gmail.com>
2022-05-16 11:37:15 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tuto-angular';
+  // 2 ways Binding
+  public title: string = 'Bonjour les gens';
+
+  public name: string = 'John Doe';
+
+  public age: number = 32;
+
+  public notes: Array<number> = [12, 13, 18, 9, 7];
 }
