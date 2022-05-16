25c1c9760818968cef441f44869762923ff16a0f
David Jegat <david.jegat@gmail.com>
2022-05-16 12:42:06 +0200

    Type todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+type Todo = {
+  id: number;
+  label: string;
+  done: boolean;
+};
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -8,14 +14,23 @@ import { Component, OnInit } from '@angular/core';
 export class TodoListComponent implements OnInit {
   public newTodo: string = '';
 
-  public todoList: Array<string> = ['Faire les courses'];
+  public todoList: Array<Todo> = [
+    { id: 1, label: 'Faire les courses', done: false },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onAddTodo(): void {
-    this.todoList.push(this.newTodo);
+    this.todoList.push({
+      id: this.todoList.length + 1,
+      label: this.newTodo,
+      done: false,
+    });
+
     this.newTodo = '';
   }
+
+  deleteTodo(todo: Todo): void {}
 }
